c749664e0c8e10b1f9fd46ffbba4916b59308770
damsx1 <dams@haadsam.com>
2024-09-07 21:14:50 +0200

    modified:   core/__pycache__/settings.cpython-311.pyc 	modified:   core/settings.py 	modified:   db.sqlite3 	deleted:    staticfiles/css/style.css.map 	modified:   templates/dashboard/dashboard_template.html 	modified:   templates/main/index.html 	modified:   templates/main/login.html

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', "127.0.0.1", 'garage.haadsam.com']
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+
 
 # Application definition
 
